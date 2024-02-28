afc5d513797b7f183ae3526c013be8031ce90ee0
yunin-s <yunin2876@mail.ru>
2024-02-28 22:40:24 +0700

    ntg

--- db/migrate/20240228140011_add_products.rb
+++ db/migrate/20240228140011_add_products.rb
@@ -8,27 +8,27 @@ class AddProducts < ActiveRecord::Migration[7.1]
       :is_spicy => false, 
       :is_veg => false,
       :is_best_offer => false,
-      :path_to_image => '/images/hawai.jpg'
+      :path_to_image => '/images/Hawai.jpg'
 )}
 Product.create ({
-  :title => 'peperoni', 
-  :desription =>'This is Hawaiian pizza',
+  :title => 'Peperoni', 
+  :desription =>'This is Peperoni pizza',
   :price => 450,
   :size => 30,
   :is_spicy => false,
   :is_veg => false,
   :is_best_offer => true,
-  :path_to_image => '/images/peperoni.png'
+  :path_to_image => '/images/Peperoni.png'
 )}
   Product.create ({
-    :title => 'veg', 
-    :desription =>'This is Hawaiian pizza',
+    :title => 'Veg', 
+    :desription =>'This is Vegetarian pizza,
     :price => 400,
     :size => 30,
     :is_spicy => false,
     :is_veg => true,
     :is_best_offer => false,
-    :path_to_image => '/images/veg.jpg'
+    :path_to_image => '/images/Veg.jpg'
     )}
   end
 end
