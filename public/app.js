6509c590f3f67ccd0c2811424cfda1e8f0ad1997
yunin-s <yunin2876@mail.ru>
2024-02-29 00:30:37 +0700

    update

--- public/app.js
+++ public/app.js
@@ -10,4 +10,11 @@ function something()
 	alert(x);
 }
  
+ function add_to_cart(id) 
+ {
+ 	var key='product_ '+ id;
+	var x = window.localStorage.getItem(key);
+ 	x = x * 1 + 1;
+ 	window.localStorage.setItem(key, x); 
+ }
 
