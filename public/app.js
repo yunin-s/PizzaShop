9c11690acf9f970cd2e8cb783f5d6d52e095ed1e
yunin-s <yunin2876@mail.ru>
2024-02-29 17:17:26 +0700

    add function

--- public/app.js
+++ public/app.js
@@ -18,3 +18,19 @@ function something()
  	window.localStorage.setItem(key, x); 
  }
 
+function cart_get_number_of_items ()
+{
+	var cnt =0 ;
+	for (var i = 0; i < window.localStorage.length;i++)
+	{
+
+		var key = window.localStorage.key(i);  // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение аналог в руби hh[key]=x
+		if (key.indexOf('product_') ==0 )
+		{
+			cnt=cnt + value*1 ;
+		}
+
+ 	}
+ 	return cnt;
+ }	
