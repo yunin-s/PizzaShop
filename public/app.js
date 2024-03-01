6f62d9b6210b0cba29b7b9919b82f93cb15db5d9
yunin-s <yunin2876@mail.ru>
2024-03-01 11:05:44 +0700

    update

--- public/app.js
+++ public/app.js
@@ -16,6 +16,8 @@ function something()
 	var x = window.localStorage.getItem(key);
  	x = x * 1 + 1;
  	window.localStorage.setItem(key, x); 
+ 	alert('Item in your cart: ' + cart_get_number_of_items());
+ 	update_orders_input();
  }
 
 function cart_get_number_of_items ()
@@ -33,4 +35,32 @@ function cart_get_number_of_items ()
 
  	}
  	return cnt;
+ }	
+
+
+function update_orders_input()
+
+{
+
+	var orders = cart_get_orders;
+	$('update_orders_input').val(orders);
+
+}
+
+
+ function cart_get_orders()
+{
+	var orders = '' ;
+	for (var i = 0; i < window.localStorage.length;i++)
+	{
+
+		var key = window.localStorage.key(i);  // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение аналог в руби hh[key]=x
+		if (key.indexOf('product_') ==0 )
+		{
+			orders=orders + key +'='+ value + ',' ;
+		}
+
+ 	}
+ 	return orders;
  }	
